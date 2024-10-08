6c4b2b597f9809ce6236f3d59e21e2a4a83a0b65
Srini <ssunda03@calpoly.edu>
2024-10-08 13:42:46 -0700

    [ss] script

--- ee471-codebase/lab2/lab2_2.py
+++ ee471-codebase/lab2/lab2_2.py
@@ -1,1 +1,24 @@
 # les go
+
+import sys
+import os
+
+# Add the 'classes' directory to the PYTHONPATH
+sys.path.append(os.path.join(os.path.dirname(__file__), '../classes'))
+
+from Robot import Robot
+
+def main():
+    robot = Robot()
+    joint_angles1 = [0, 0, 0, 0]
+    joint_angles2 = [15, -45, -60, 90]
+    joint_angles3 = [-90, 15, 30, -45]
+    
+    print(robot.get_fk(joint_angles1))
+    print("\n")
+    print(robot.get_fk(joint_angles2))
+    print("\n")
+    print(robot.get_fk(joint_angles3))
+
+if __name__ == "__main__":
+    main()
