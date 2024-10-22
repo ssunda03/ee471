c2da211da7fba6c8dc9fc47bb4c2432e09e3ee80
Srini <ssunda03@calpoly.edu>
2024-10-22 14:40:18 -0700

    [ss] first two plots

--- ee471-codebase/lab4/lab4_2.py
+++ ee471-codebase/lab4/lab4_2.py
@@ -38,6 +38,22 @@ def plot_3d_trajectory(poses):
     plt.title('3D Trajectory of End-Effector')
     plt.show()
 
+def plot_time_series_of_joint_angles(data):
+    plt.plot(data["time"], data["joints"][:,0], label='Joint Angle q1', linestyle='-', color='b')
+    plt.plot(data["time"], data["joints"][:,1], label='Joint Angle q2', linestyle='--', color='g')
+    plt.plot(data["time"], data["joints"][:,2], label='Joint Angle q3', linestyle='-.', color='r')
+    plt.plot(data["time"], data["joints"][:,3], label='Joint Angle q4', linestyle=':', color='m')
+
+    # Adding labels and title
+    plt.title('Joint Angles Over Time')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Joint Angles (degrees)')
+    plt.legend()
+    plt.grid(True)
+
+    # Show the plot
+    plt.show()
+
 def init_robot(robot, traj_init):
     robot.write_time(traj_init)  # Write trajectory time
     robot.write_motor_state(True)  # Write position mode
@@ -48,64 +64,73 @@ def init_robot(robot, traj_init):
     time.sleep(traj_init)  # Wait for trajectory completion
 
 def main():
-    # Initialize Robot instance
-    robot = Robot()
-    traj_init = 3 # Traj time
-    init_robot(robot, traj_init)
+    # # Initialize Robot instance
+    # robot = Robot()
+    # traj_init = 3 # Traj time
+    # init_robot(robot, traj_init)
 
-    # Setpoints for trajectory
-    setpoints_taskspace = [
-        np.array([25, -100, 150, -60]),
-        np.array([150, 80, 300, 0]),
-        np.array([250, -115, 75, -45]),
-        np.array([25, -100, 150, -60]),
-    ]
+    # # Setpoints for trajectory
+    # setpoints_taskspace = [
+    #     np.array([25, -100, 150, -60]),
+    #     np.array([150, 80, 300, 0]),
+    #     np.array([250, -115, 75, -45]),
+    #     np.array([25, -100, 150, -60]),
+    # ]
 
-    # Use IK method to get Joint Angles
-    setpoints_jointspace = []
-    for i in range(len(setpoints_taskspace)):
-        cur_joints = robot.get_ik(setpoints_taskspace[i])
-        setpoints_jointspace.append(cur_joints)
+    # # Use IK method to get Joint Angles
+    # setpoints_jointspace = []
+    # for i in range(len(setpoints_taskspace)):
+    #     cur_joints = robot.get_ik(setpoints_taskspace[i])
+    #     setpoints_jointspace.append(cur_joints)
 
-    # Make np array
-    setpoints_jointspace = np.array(setpoints_jointspace)
+    # # Make np array
+    # setpoints_jointspace = np.array(setpoints_jointspace)
 
-    # Create instance of TrajPlanner
-    planner = TrajPlanner(setpoints_jointspace)
+    # # Create instance of TrajPlanner
+    # planner = TrajPlanner(setpoints_jointspace)
 
-    # Parameters for cubic trajectory
-    traj_time = 5  # 5 seconds between setpoints
-    num_waypoints = 998  # 998 intermediate waypoints
+    # # Parameters for cubic trajectory
+    # traj_time = 5  # 5 seconds between setpoints
+    # num_waypoints = 998  # 998 intermediate waypoints
 
-    # Generate cubic trajectory for each segment: Pose 1 -> Pose 2 -> Pose 3 -> Pose 1
-    trajectory = planner.get_cubic_traj(traj_time, num_waypoints)
-    time_step = trajectory[2,0] - trajectory[1,0]
+    # # Generate cubic trajectory for each segment: Pose 1 -> Pose 2 -> Pose 3 -> Pose 1
+    # trajectory = planner.get_cubic_traj(traj_time, num_waypoints)
+    # time_step = trajectory[2,0] - trajectory[1,0]
 
-    # Pre-allocate data
-    data_time = np.zeros(num_waypoints+2)
-    data_ee_poses = np.zeros((num_waypoints+2, 4))
-    data_q = np.zeros((num_waypoints+2, 4))
-    count = 0
+    # # Pre-allocate data
+    # data_time = np.zeros(num_waypoints+2)
+    # data_ee_poses = np.zeros((num_waypoints+2, 4))
+    # data_q = np.zeros((num_waypoints+2, 4))
+    # count = 0
 
-    robot.write_time(time_step)
-    start_time = time.time()
+    # robot.write_time(time_step)
+    # start_time = time.time()
 
-    # Move the robot along all trajectories
-    for i in range(1, len(trajectory)):
-        robot.write_joints(trajectory[i, 1:])  # Write joint values
-        # Collect a reading periodically until the waypoint is reached
-        while time.time() - start_time < (i * time_step):
-            data_q[count, :] = robot.get_joints_readings()[0, :]
-            data_time[count] = time.time() - start_time
-            data_ee_poses[count, :] = robot.get_ee_pos(data_q[count, :])[0:4]
-            count += 1
+    # # Move the robot along all trajectories
+    # for i in range(1, len(trajectory)):
+    #     robot.write_joints(trajectory[i, 1:])  # Write joint values
+    #     # Collect a reading periodically until the waypoint is reached
+    #     while time.time() - start_time < (i * time_step):
+    #         data_q[count, :] = robot.get_joints_readings()[0, :]
+    #         data_time[count] = time.time() - start_time
+    #         data_ee_poses[count, :] = robot.get_ee_pos(data_q[count, :])[0:4]
+    #         count += 1
 
-    # Trim unused space in data
-    data_time = data_time[:count]
-    data_ee_poses = data_ee_poses[:count, :]
-    data_q = data_q[:count, :]
+    # # Trim unused space in data
+    # data_time = data_time[:count]
+    # data_ee_poses = data_ee_poses[:count, :]
+    # data_q = data_q[:count, :]
+    
+    # data = {
+    #     "joints" : data_q,
+    #     "ee" : data_ee_poses,
+    #     "time" : data_time
+    # }
+    
+    data = load_from_pickle("lab4_cubic_traj.pkl")
 
-    plot_3d_trajectory(data_ee_poses)
+    plot_3d_trajectory(data["ee"])
+    plot_time_series_of_joint_angles(data)
 
 
     # for waypoint in range(len(trajectory) - 1):  # Loop over waypoints in the segment
